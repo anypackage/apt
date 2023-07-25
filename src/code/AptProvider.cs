2dbc5efb5127571c8fb6d20ca9e80143efcfaa03
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2023-07-24 20:24:30 -0500

    Add Find-Package

--- src/code/AptProvider.cs
+++ src/code/AptProvider.cs
@@ -2,50 +2,150 @@
 // You may use, distribute and modify this code under the
 // terms of the MIT license.
 
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
 
 namespace AnyPackage.Provider.Apt
 {
     [PackageProvider("Apt")]
-    public sealed class AptProvider : PackageProvider, IGetPackage
+    public sealed class AptProvider : PackageProvider, IGetPackage, IFindPackage
     {
         private static readonly AptVersionComparer s_comparer = new AptVersionComparer();
 
         public void GetPackage(PackageRequest request)
         {
+            foreach (var package in GetPackages())
+            {
+                if (IsMatch(package, request))
+                {
+                    request.WritePackage(package);
+                }
+            }
+        }
+
+        public void FindPackage(PackageRequest request)
+        {
+            foreach (var package in FindPackages(request))
+            {
+                if (IsMatch(package, request))
+                {
+                    request.WritePackage(package);
+                }
+            }
+        }
+
+        private IEnumerable<PackageInfo> GetPackages()
+        {
             using var process = new Process();
-            process.StartInfo.Arguments = "-W -f=${binary:Package};${Version};${binary:Summary};${db:Status-Status}\n";
+            process.StartInfo.Arguments = "-s";
             process.StartInfo.FileName = "dpkg-query";
             process.StartInfo.CreateNoWindow = true;
             process.StartInfo.RedirectStandardOutput = true;
             process.Start();
-
             using var reader = process.StandardOutput;
-            string? text;
 
-            while ((text = reader.ReadLine()) is not null)
+            foreach (var package in ParsePackage(reader))
             {
-                ProcessOutput(text, request);
+                if (package.Metadata.ContainsKey("Status")
+                    && Regex.IsMatch((string)package.Metadata["Status"], "installed"))
+                {
+                    yield return package;
+                }
             }
         }
 
-        private void ProcessOutput(string? text, PackageRequest request)
+        private IEnumerable<PackageInfo> FindPackages(PackageRequest request)
         {
-            if (text is null)
+            using var process = new Process();
+            process.StartInfo.Arguments = "search . --full";
+            process.StartInfo.FileName = "apt-cache";
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+            using var reader = process.StandardOutput;
+
+            foreach (var package in ParsePackage(reader))
             {
-                return;
+                yield return package;
+            }
+        }
+
+        private bool IsMatch(PackageInfo package, PackageRequest request)
+        {
+            return request.IsMatch(package.Name)
+                   && (request.Version is null || (package.Version is not null
+                                                   && request.Version.Satisfies(package.Version, s_comparer)));
+        }
+
+        private IEnumerable<PackageInfo> ParsePackage(StreamReader reader)
+        {
+            string? line;
+            var first = true;
+            var dictionary = new Dictionary<string, object>();
+
+            while ((line = reader.ReadLine()) is not null)
+            {
+                var match = Regex.Match(line, "^Package: (?<Package>.+)$");
+
+                if (match.Success)
+                {
+                    if (!first)
+                    {
+                        yield return new PackageInfo((string)dictionary["Package"],
+                                                     (string)dictionary["Version"],
+                                                     null,
+                                                     GetDescription(dictionary),
+                                                     null,
+                                                     dictionary,
+                                                     ProviderInfo);
+                    }
+                    else
+                    {
+                        first = false;
+                    }
+
+                    dictionary = new Dictionary<string, object>();
+                    dictionary.Add("Package", match.Groups["Package"].Value);
+                    continue;
+                }
+
+                match = Regex.Match(line, @"^(?<Key>\S+): (?<Value>.+)$");
+
+                if (match.Success)
+                {
+                    dictionary.Add(match.Groups["Key"].Value, match.Groups["Value"].Value);
+                }
             }
 
-            var values = text.Split(';');
-
-            if (values[3] == "installed"
-                && request.IsMatch(values[0])
-                && (request.Version is null
-                    || request.Version.Satisfies(values[1], s_comparer)))
+            if (!first)
             {
-                var package = new PackageInfo(values[0], values[1], values[2], ProviderInfo);
-                request.WritePackage(package);
+                yield return new PackageInfo((string)dictionary["Package"],
+                                             (string)dictionary["Version"],
+                                             null,
+                                             GetDescription(dictionary),
+                                             null,
+                                             dictionary,
+                                             ProviderInfo);
             }
+        }
+
+        private string GetDescription(Dictionary<string, object> dictionary)
+        {
+            var description = string.Empty;
+
+            if (dictionary.ContainsKey("Description"))
+            {
+                description = (string)dictionary["Description"];
+            }
+            else if (dictionary.ContainsKey("Description-en"))
+            {
+                description = (string)dictionary["Description-en"];
+            }
+
+            return description;
         }
     }
 }
